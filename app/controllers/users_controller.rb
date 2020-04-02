b7cad694dccb445e7e8037820da5f2d781566bbf
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-04-02 16:55:33 -0400

    Configures create and show methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to signup_path
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   private
