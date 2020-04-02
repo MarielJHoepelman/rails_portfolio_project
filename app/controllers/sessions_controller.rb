1707d015a733152930a69212c9d6e556d28cb958
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-04-02 18:51:04 -0400

    Configures create and session_params methods.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    @user = User.find_by(email: params[:session][:email]) unless params[:session][:email].blank?
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to login_path
+    end
   end
 
   def destroy
     session.clear
     redirect_to root_path
+  end
+
+  private
+
+  def session_params
+      params.require(:session).permit(:email, :password)
   end
 end
