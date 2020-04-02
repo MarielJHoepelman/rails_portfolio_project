e25edc4cce28ad9e5220c72b22be584e616d3997
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-04-02 15:54:56 -0400

    Creates Users controller with basic CRUD methods.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email)
+  end
+
+end
