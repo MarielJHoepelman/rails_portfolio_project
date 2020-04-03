187a0df5662e14438a67652216ef4e0e1ad0493e
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-04-03 11:59:15 -0400

    Creates current_user method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!session[:user_id]
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) unless session[:user_id].blank?
+  end
 end
