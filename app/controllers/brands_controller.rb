afa03e6c30d1a5c829896242574fcad29fe03966
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-04-06 18:30:00 -0400

    Adds basic CRUD methods.

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,2 +1,20 @@
 class BrandsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @brand = Brand.create(brand_params)
+    redirect_to brand_path(@brand)
+  end
+
+  def show
+    @brand = Brand.find(params[:id])
+  end
+
+  private
+
+  def brand_params
+    params.require(:brand).permit(:name)
+  end
 end
