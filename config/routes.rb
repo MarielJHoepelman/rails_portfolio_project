09739cf4be9ad5c0b2c24a867deb0130e8b07d0b
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-04-06 16:33:57 -0400

    Generates Categories controller, model, migration and helper file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   resources :brands
   resources :user_products
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_202506) do
+ActiveRecord::Schema.define(version: 2020_04_06_203101) do
 
   create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
