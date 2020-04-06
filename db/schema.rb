4c410616a2375a8f99cae9ca706664e5c5c99313
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-04-06 16:29:41 -0400

    Generates Brands controller, model, migration and helper file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_201619) do
+ActiveRecord::Schema.define(version: 2020_04_06_202506) do
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "user_products", force: :cascade do |t|
     t.integer "user_id"
