431509a89e263a4ddd4ee19fd91fdb401e397a01
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-05-04 17:57:02 -0400

    Adds validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :products, through: :user_products
+  
   has_secure_password
+
+  validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true
 end
