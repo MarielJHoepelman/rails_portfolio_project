9e6532645c20b8cf97a77fb5a87ade8c9183db38
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-05-05 16:11:51 -0400

    Adds redirect_if_logged_in and current_user_is_params_user methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,15 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) unless session[:user_id].blank?
   end
+
+  def redirect_if_logged_in
+    if logged_in?
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def current_user_is_params_user?
+    current_user.id != params[:id].to_i
+  end
+
 end
