f7c4f82f9cebb6841bcacee50e354300eda34e43
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-05-05 16:08:30 -0400

    Deletes ununsed resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :products
   resources :categories
-  resources :brands
   resources :user_products
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'sessions#home'
+
+  # get '/profile' => 'users#show'
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
