e6438850ec23c4866a1474902a1fbf58ad4197ab
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-05-06 19:52:30 -0400

    Adds index, new and create actions. Adds user_products_params method.

--- app/controllers/user_products_controller.rb
+++ app/controllers/user_products_controller.rb
@@ -1,2 +1,27 @@
 class UserProductsController < ApplicationController
+
+  def index
+    @category = Category.find(params[:category_id])
+    @user = User.find(params[:user_id])
+    @user_products = UserProduct.joins(:product)
+      .where("user_products.user_id=? and products.category_id=?", params[:user_id], params[:category_id])
+  end
+
+  def new
+    @product = Product.find(params[:product_id])
+    @user_product = UserProduct.new(product_id: @product.id, user_id: current_user.id)
+  end
+
+  def create
+    @user_product = UserProduct.create(user_products_params)
+
+    redirect_to user_category_user_products_path(@user_product.user, @user_product.product.category)
+  end
+
+  private
+
+  def user_products_params
+    params.require(:user_product).permit(:user_id, :product_id, :expiration_date, :price)
+  end
+
 end
