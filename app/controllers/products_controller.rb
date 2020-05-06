1348badd3b0735806d0aa95bde7974c420e2bfa5
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-05-06 19:51:22 -0400

    Adds new, create, index and show actions. Creates private products_params methods.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,25 @@
 class ProductsController < ApplicationController
+
+    def new
+      @categories = Category.all
+    end
+
+    def create
+      @product = Product.create(product_params)
+      redirect_to new_product_user_product_path(@product)
+    end
+
+    def index
+      @products = Product.all
+    end
+
+    def show
+      @product = Product.find(params[:id])
+    end
+
+    private
+
+    def product_params
+      params.require(:product).permit(:name, :category_id)
+    end
 end
