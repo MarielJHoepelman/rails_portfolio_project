992639f611b8c26776f94d9d86b56fb4042c8b39
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-05-06 20:02:24 -0400

    Adds nested resources to users, products and categories routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :categories
+
+  resources :products do
+    resources :user_products
+  end
+
+  resources :categories do
+    resources :products
+  end
+
   resources :user_products
-  resources :users
+
+  resources :users do
+    resources :categories do
+      resources :user_products, only: :index
+    end
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'sessions#home'
 
