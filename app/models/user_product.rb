9b3a3fbe1641c226c8e5cbeeb53fa4888d22a702
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-05-30 14:21:40 -0400

    Creates scope method.

--- app/models/user_product.rb
+++ app/models/user_product.rb
@@ -1,4 +1,10 @@
 class UserProduct < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+   scope :product_from_category, ->(user_id, category_id) { joins(:product).where("user_products.user_id=? and products.category_id=?", user_id, category_id) }
+
+  # def self.product_from_category(user_id, category_id)
+  #   joins(:product).where("user_products.user_id=? and products.category_id=?", user_id, category_id)
+  # end
 end
