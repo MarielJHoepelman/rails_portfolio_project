84084d0d197598b6bc42a35f971f0d8715970a23
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-06-01 18:05:46 -0400

    Refactors controller actions.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,25 @@
 class ProductsController < ApplicationController
+  def new
+    @categories = Category.all
+    @category = Category.find(params[:id])
+  end
 
-    def new
-      @categories = Category.all
-      @category = Category.find(params[:id])
-    end
+  def create
+    @product = Product.create(product_params)
+    redirect_to new_product_user_product_path(@product)
+  end
 
-    def create
-      @product = Product.create(product_params)
-      redirect_to new_product_user_product_path(@product)
-    end
+  def index
+    @products = Product.where(category_id: params[:category_id])
+  end
 
-    def index
-      @products = Product.all
-    end
+  def show
+    @product = Product.find(params[:id])
+  end
 
-    def show
-      @product = Product.find(params[:id])
-    end
+  private
 
-    private
-
-    def product_params
-      params.require(:product).permit(:name, :category_id)
-    end
+  def product_params
+    params.require(:product).permit(:name, :category_id)
+  end
 end
