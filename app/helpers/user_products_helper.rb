e6fc23b93683cf0578e6862bfb1c123ecd3022ea
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-06-17 12:32:02 -0400

    Creates message and value of collection methods.

--- app/helpers/user_products_helper.rb
+++ app/helpers/user_products_helper.rb
@@ -1,2 +1,19 @@
 module UserProductsHelper
+
+  def user_products_message(counter)
+    case counter
+    when 0
+      "You don't have any products in your collection."
+    when 1
+      "You have one product in your collection!"
+    else
+      "You have #{counter} products in your collection!"
+    end
+  end
+
+  def value_of_collection (user_products)
+    total_value = user_products.reduce(0) {|sum, user_product| sum + user_product.price }
+    "The total value of your collection is $%.2f" % total_value
+  end
+
 end
