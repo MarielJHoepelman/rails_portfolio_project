a31eb51bfd7637683c693d4507b01e0e5c58e2b4
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-06-23 17:11:25 -0400

    Adds price presence and numericality validations.

--- app/models/user_product.rb
+++ app/models/user_product.rb
@@ -2,6 +2,9 @@ class UserProduct < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
+  validates :price, presence: true
+  validates :price, numericality: true
+
    scope :product_from_category, ->(user_id, category_id) { joins(:product).where("user_products.user_id=? and products.category_id=?", user_id, category_id) }
 
   # def self.product_from_category(user_id, category_id)
