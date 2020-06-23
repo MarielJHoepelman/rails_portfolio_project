da742c54ae35f6a7f5986dec4e6f272534f63e44
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-06-23 17:10:14 -0400

    Creates edit and update actions. Creates find product and redirect to index methods.

--- app/controllers/user_products_controller.rb
+++ app/controllers/user_products_controller.rb
@@ -1,4 +1,5 @@
 class UserProductsController < ApplicationController
+  before_action :find_product, only: [:edit, :update, :destroy]
 
   def index
     @category = Category.find(params[:category_id])
@@ -13,19 +14,40 @@ class UserProductsController < ApplicationController
 
   def create
     @user_product = UserProduct.create(user_products_params)
-    redirect_to user_category_user_products_path(@user_product.user, @user_product.product.category)
+    if @user_product.valid?
+      redirect_to_user_products_index
+    else
+      flash[:notice] = "Please fill price field"
+
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @user_product.price = params[:user_product][:price]
+    @user_product.save
+    redirect_to_user_products_index
   end
 
   def destroy
-    @user_product = UserProduct.find(params[:id])
     @user_product.destroy
-    redirect_to user_category_user_products_path(current_user, @user_product.product.category)
+    redirect_to_user_products_index
   end
 
   private
 
   def user_products_params
-    params.require(:user_product).permit(:user_id, :product_id, :expiration_date, :price)
+    params.require(:user_product).permit(:user_id, :product_id, :price)
   end
 
+  def find_product
+    @user_product = UserProduct.find(params[:id])
+  end
+
+  def redirect_to_user_products_index
+    redirect_to user_category_user_products_path(current_user, @user_product.product.category)
+  end
 end
