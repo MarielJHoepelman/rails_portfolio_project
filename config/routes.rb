73a057a558f866737858fa16b2ed15338bab9016
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-20 23:10:35 -0400

    Refactors routes to keep only ones controllers use.

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,31 @@
 Rails.application.routes.draw do
 
-  resources :products do
-    resources :user_products, only: [:new, :index, :edit, :create]
-  end
-
-  resources :user_products, only: [:create, :destroy, :update ]
-
-  resources :categories do
-    resources :products
-  end
-
-  resources :users do
-    resources :categories do
-      resources :user_products, only: [:index, :edit]
-    end
-  end
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'sessions#home'
 
-  # get '/profile' => 'users#show'
+  get 'home' => 'users#show', :as => :home
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-
-  # log in with Google
+  delete 'logout' => 'sessions#destroy'
 
   get '/auth/google_oauth2/callback' => 'sessions#google'
 
-  delete 'logout' => 'sessions#destroy'
+#for adding an existing product to an user product collection
+  resources :products, only: [:new, :create] do
+    resources :user_products, only: [:new, :create]
+  end
 
+#for editing and deleting an user product.
+  resources :user_products, only: [:destroy, :update]
+
+#creates a new category.
+#creates new product under category.
+#shows user product list.
+  resources :categories, only: [:new, :create] do
+    resources :products, only: [:new, :index, :create]
+    resources :user_products, only: [:index, :edit]
+  end
 end
