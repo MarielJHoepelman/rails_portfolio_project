b37d5021856549caf4e161eab3f78a1786911e87
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-08-20 23:04:51 -0400

    Removes unused method.

--- app/models/user_product.rb
+++ app/models/user_product.rb
@@ -5,9 +5,7 @@ class UserProduct < ApplicationRecord
   validates :price, presence: true
   validates :price, numericality: true
 
-   scope :product_from_category, ->(user_id, category_id) { joins(:product).where("user_products.user_id=? and products.category_id=?", user_id, category_id) }
-
-  # def self.product_from_category(user_id, category_id)
-  #   joins(:product).where("user_products.user_id=? and products.category_id=?", user_id, category_id)
-  # end
+  scope :product_from_category, ->(user_id, category_id) do
+    joins(:product).where("user_products.user_id=? and products.category_id=?", user_id, category_id)
+  end
 end
